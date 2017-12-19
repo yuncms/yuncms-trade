3fb91a57cb183c7c48560d25b2a59ac81949b569
Peter <xutongle@gmail.com>
2017-12-19 21:16:53 +0800

    update

--- frontend/views/trade/pay.php
+++ frontend/views/trade/pay.php
@@ -1,11 +1,10 @@
 <?php
+
+use xutl\jquery\qrcode\QRCodeAsset;
+
+QRCodeAsset::register($this);
+
 /* @var \yii\web\View $this */
-//二维码付款
-$this->registerJsFile('https://www.helloweba.com/demo/qrcode/jquery.qrcode.min.js',[
-    'depends'=>['yii\web\JqueryAsset']
-]);
-
-
 
 
 $this->registerJs('$(\'#code\').qrcode("'.$paymentParams['qr_code'].'");')
