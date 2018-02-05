1e8ad2a3c00b244be03371f16eeea4bd22f37715
xutongle <xutongle@gmail.com>
2018-02-05 19:36:51 +0800

    交易通知

---
+++ notifications/TradeNotification.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\trade\notifications;
+
+use yuncms\notification\Notification;
+
+/**
+ * Class TradeNotification
+ * @package yuncms\trade\notifications
+ *
+ * @author Tongle Xu <xutongle@gmail.com>
+ * @since 1.0
+ */
+class TradeNotification extends Notification
+{
+    public function getTitle()
+    {
+        
+    }
+}
